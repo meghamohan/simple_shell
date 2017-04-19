641524be8ae9585a872b6c9710f57beafbd54693
Halina Veratsennik <121@holbertonschool.com>
2017-04-19 20:49:47 +0000

    updated Betty style

--- builtinFunctions.c
+++ builtinFunctions.c
@@ -1,20 +1,25 @@
 #include "shell.h"
 
+/**
+ * getMyBuiltins - function to search through built in if input match
+ *@h: pointer to head
+ *@cmds: pointer to array of commands
+ * Return: pointer to function if command match, 0 - otherwise
+ */
+
 int getMyBuiltins(env **h, char **cmds)
 {
 	int i;
 
 	builtinFunc builtIns [] = {
-/*		{"cd", changeMyDir},
-		{"env", _getenv},*/
 		{"setenv", _setEnv},
 		{"env", print_env_list},
 		{NULL, NULL}
 	};
 	for (i = 0; builtIns[i].cmd != NULL; i++)
 	{
-		if(_strcmp(cmds[0], builtIns[i].cmd) == 0)
-			return(builtIns[i].f(h, cmds));
+		if (_strcmp(cmds[0], builtIns[i].cmd) == 0)
+			return (builtIns[i].f(h, cmds));
 	}
 	return (0);
 }
